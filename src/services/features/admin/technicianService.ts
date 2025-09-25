678f4c2fa49bdb45974bbe2c6cdacc6504bb8504
Be4r1908 <truong1908thd@gmail.com>
2025-09-25 10:19:28 +0700

    call api center service

--- src/services/features/admin/technicianService.ts
+++ src/services/features/admin/technicianService.ts
@@ -3,8 +3,12 @@ import { STAFF_LIST_ENDPOINT } from '../../constant/apiConfig';
 
 export interface StaffUser {
   _id: string;
+  username?: string;
   email: string;
   fullName?: string;
+  phone?: string;
+  address?: string;
+  isVerified?: boolean;
   role: string;
 }
 
@@ -13,5 +17,22 @@ export const fetchTechnicians = async (): Promise<StaffUser[]> => {
   const allStaff = res.data?.staff || res.data;
   return Array.isArray(allStaff)
     ? allStaff.filter((u) => u.role === 'technician')
+    : [];
+};
+
+
+export const fetchAllStaff = async (): Promise<StaffUser[]> => {
+  const res = await axiosInstance.get(STAFF_LIST_ENDPOINT);
+  const allStaff = res.data?.staff || res.data;
+  return Array.isArray(allStaff)
+    ? allStaff
+    : [];
+};
+
+export const fetchStaff = async (): Promise<StaffUser[]> => {
+  const res = await axiosInstance.get(STAFF_LIST_ENDPOINT);
+  const allStaff = res.data?.staff || res.data;
+  return Array.isArray(allStaff)
+    ? allStaff.filter((u) => u.role === 'staff')
     : [];
 };
